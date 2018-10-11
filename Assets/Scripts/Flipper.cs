3b4b17e76e0c3beb3c9408273fda92b556069c33
Eric Roberts <eric@threedot14.com>
2018-10-10 21:17:03 -0400

    Add simple flipper

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(new Vector3(0, 0, Time.deltaTime * 150), Space.Self);
+        }
+        else if (Input.GetKey("right"))
+        {
+            transform.Rotate(new Vector3(0, 0, Time.deltaTime * -150), Space.Self);
+        }
+    }
+}
