3bdfb46fd46f7684803ddfe05d8c5c4bf99a54d6
Eric Roberts <eric@threedot14.com>
2018-10-16 20:58:20 -0400

    Start flipper logic

--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -3,21 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flipper : MonoBehaviour {
+    public float pushForce;
+
+    private Rigidbody2D rigidbody2D;
+
+    private bool running = false;
+    private float startTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("left"))
+        if (running)
         {
-            transform.Rotate(new Vector3(0, 0, Time.deltaTime * 150), Space.Self);
+            float delta = Time.time - startTime;
+            if (delta > 0.6)
+            {
+                running = false;
+                rigidbody2D.MoveRotation(0);
+            }
+            else
+            {
+                rigidbody2D.MoveRotation(90 * Easing(delta / 0.6f));
+            }
         }
-        else if (Input.GetKey("right"))
+        else
         {
-            transform.Rotate(new Vector3(0, 0, Time.deltaTime * -150), Space.Self);
+            if (Input.GetKeyDown("right"))
+            {
+                running = true;
+                startTime = Time.time;
+            }
+        }
+    }
+
+    float Easing(float p)
+    {
+        if ((1-p) < 0.4)
+        {
+            return Mathf.Exp(1.733f * (1-p)) - 1;
+        }
+        else
+        {
+            return Mathf.Exp(-11.5129f * (1-p) + 4.60517f) - 0.001f;
         }
     }
 }
