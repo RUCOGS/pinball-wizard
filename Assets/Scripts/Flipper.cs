8d54ee652498aa75b308ea6d895576f29754a16f
Eric Roberts <eric@threedot14.com>
2018-11-27 20:59:12 -0500

    Add more accurate flipper

--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -7,61 +8,51 @@ public class Flipper : MonoBehaviour
 {
     public KeyCode triggerKey;
     public float degreeChange;
-    public float rotationTime;
+    public float forwardAcceleration;
+    public float backwardAcceleration;
     
     private Rigidbody2D rigidbody2D;
     
     private float startAngle;
-    private float coefficient;
-    
-    private bool running = false;
-    private float startTime = 0.0f;
+    private float endAngle;
+    private float currentAngle;
+    private float velocity = 0.0f;
 
 	private void Start () {
         rigidbody2D = GetComponent<Rigidbody2D>();
 	    
-	    // startAngle and coefficient are calculated on Start from the public values, and
-	    // the beginning angle of the object
+	    
 	    startAngle = transform.eulerAngles.z;
-	    coefficient = degreeChange / rotationTime;
+	    endAngle = startAngle + degreeChange;
+	    currentAngle = startAngle;
 	}
 
     private void Update()
     {
-        if (running)
+        float accel;
+        if (Input.GetKey(triggerKey))
         {
-            float angle;
-            var  delta = Time.time - startTime;
-            if (delta > rotationTime)
-            {
-                running = false;
-                angle = startAngle;
-            }
-            else
-            {
-                angle = startAngle + coefficient * Easing(delta / rotationTime);
-            }
-            rigidbody2D.MoveRotation(angle);
-        }
-        else if (Input.GetKeyDown(triggerKey))
-        {
-            running = true;
-            startTime = Time.time;
-        }
-    }
-
-    /**
-     *  Takes a number  0-1, and gives a number 0-1
-     */
-    private static float Easing(float p)
-    {
-        if (p < 0.5)
-        {
-            return 2 * p;
+            accel = forwardAcceleration;
         }
         else
         {
-            return 2 - 2 * p;
+            accel = backwardAcceleration;
         }
+
+        velocity += accel;
+        currentAngle += velocity;
+        
+        // Depending on startAngle and endAngle some extra logic is necessary if we cross over 360 degrees
+        if (currentAngle < startAngle)
+        {
+            currentAngle = startAngle;
+            velocity = 0;
+        } else if (currentAngle > endAngle)
+        {
+            currentAngle = endAngle;
+            velocity = 0;
+        }
+
+        rigidbody2D.MoveRotation(currentAngle);
     }
 }
